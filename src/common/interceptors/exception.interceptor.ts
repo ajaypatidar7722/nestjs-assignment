b79f0259038a2533d0201665e011ea6d14421040
ajaypatidar7722 <ajaypatidar7722@gmail.com>
2024-04-19 18:38:11 +0530

    Better logging and response for production mode

--- src/common/interceptors/exception.interceptor.ts
+++ src/common/interceptors/exception.interceptor.ts
@@ -7,13 +7,22 @@ import {
   Logger,
   NestInterceptor,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { TypeORMError } from 'typeorm';
 
+interface ErrorResponse {
+  message: string;
+  status: number;
+  stack?: string;
+}
+
 @Injectable()
 export class ErrorsInterceptor implements NestInterceptor {
   private logger = new Logger(ErrorsInterceptor.name);
+
+  constructor(private readonly configService: ConfigService) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
@@ -25,36 +34,37 @@ export class ErrorsInterceptor implements NestInterceptor {
             return error;
           }
 
-          let message = error.message || 'Internal server error';
-          let status = error.status || HttpStatus.INTERNAL_SERVER_ERROR;
+          const response: ErrorResponse = {
+            message: error.message || 'Internal server error',
+            status: error.status || HttpStatus.INTERNAL_SERVER_ERROR,
+          };
+
+          if (this.configService.get('app.env') !== 'production') {
+            response.stack = error.stack;
+          }
 
           if (error instanceof TypeORMError) {
             switch (error.name) {
               case 'EntityNotFoundError':
-                message = 'Resource not found';
-                status = HttpStatus.NOT_FOUND;
+                response.message = 'Resource not found';
+                response.status = HttpStatus.NOT_FOUND;
                 break;
 
               case 'QueryFailedError':
-                message = 'Query failed';
-                status = HttpStatus.BAD_REQUEST;
+              case 'UpdateValuesMissingError':
+                response.message = 'Query failed';
+                response.status = HttpStatus.BAD_REQUEST;
                 break;
 
               default:
-                message = 'Query failed';
-                status = HttpStatus.INTERNAL_SERVER_ERROR;
+                response.message = 'Query failed';
+                response.status = HttpStatus.INTERNAL_SERVER_ERROR;
                 break;
             }
           }
 
-          return new HttpException(
-            {
-              message,
-              status, // TO also send the status as part of resopnse body
-              stack: error.stack,
-            },
-            status
-          );
+          this.logger.error(error);
+          return new HttpException(response, response.status);
         });
       })
     );
