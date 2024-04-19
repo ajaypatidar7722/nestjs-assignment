c4fc1e778ff0698446260e83ba6ed67960d01dd1
ajaypatidar7722 <ajaypatidar7722@gmail.com>
2024-04-19 18:15:33 +0530

    Update interceptor to throw meaningful errors

--- src/common/interceptors/exception.interceptor.ts
+++ src/common/interceptors/exception.interceptor.ts
@@ -4,22 +4,59 @@ import {
   HttpException,
   HttpStatus,
   Injectable,
+  Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { TypeORMError } from 'typeorm';
 
 @Injectable()
 export class ErrorsInterceptor implements NestInterceptor {
+  private logger = new Logger(ErrorsInterceptor.name);
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next
-      .handle()
-      .pipe(
-        catchError((err) =>
-          throwError(
-            () => new HttpException('New message', HttpStatus.BAD_GATEWAY)
-          )
-        )
-      );
+    return next.handle().pipe(
+      catchError((error) => {
+        this.logger.warn('Intercepting error');
+
+        return throwError(() => {
+          if (error instanceof HttpException) {
+            return error;
+          }
+
+          let message = error.message || 'Internal server error';
+          let status = error.status || HttpStatus.INTERNAL_SERVER_ERROR;
+
+          if (error instanceof TypeORMError) {
+            switch (error.name) {
+              case 'EntityNotFoundError':
+                message = 'Resource not found';
+                status = HttpStatus.NOT_FOUND;
+                break;
+
+              case 'QueryFailedError':
+                message = 'Query failed';
+                status = HttpStatus.BAD_REQUEST;
+                break;
+
+              default:
+                message = 'Query failed';
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+                break;
+            }
+          }
+
+          return new HttpException(
+            {
+              message,
+              status, // TO also send the status as part of resopnse body
+              stack: error.stack,
+            },
+            status
+          );
+        });
+      })
+    );
   }
 }
